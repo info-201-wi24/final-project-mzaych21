9baf6814e9a88a1db38ee6a5c2e2b14a7c714180
YunsuHa <yh254@uw.edu>
2024-03-04 20:22:52 -0700

    Resolved conflicts

--- server.R
+++ server.R
@@ -1,61 +1,4 @@
-# Run them all in the R Console.
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(plotly)
-library(shiny)
 
-<<<<<<< HEAD
-trav_and_res_df <- read.csv("trav_and_res.csv") # Set work
-  
-  server <- function(input, output){
-    
-    # TODO Make outputs based on the UI inputs here
-    output$barplot <- renderPlotly({
-      selected_team_data <- reactive({
-        if (input$team_selector == "Seattle Kraken") {
-          filtered_data <- trav_and_res_df %>%
-            summarize(
-              Sea_wins = sum(grepl("Sea W", W.or.L)),
-              Sea_losses = -sum(grepl("Sea L", W.or.L))
-            )
-        } else if (input$team_selector == "Pittsburgh Penguins") {
-          filtered_data <- trav_and_res_df %>%
-            summarize(
-              Pitt_wins = sum(grepl("Pitt W", W.or.L)),
-              Pitt_losses = -sum(grepl("Pitt L", W.or.L))
-            )
-        } else {
-          filtered_data <- trav_and_res_df %>% 
-            summarize(
-              Pitt_wins = sum(grepl("Pitt W", W.or.L)),
-              Pitt_losses = -sum(grepl("Pitt L", W.or.L)),
-              Sea_wins = sum(grepl("Sea W", W.or.L)),
-              Sea_losses = -sum(grepl("Sea L", W.or.L))
-            )
-        }
-        return(filtered_data)
-      })
-      
-      wins_losses <- reactive({
-        selected_team_data() %>%
-          pivot_longer(cols = everything(),
-                       names_to = "Category", values_to = "Games")
-      })
-      
-      barplot <- ggplot(wins_losses(), aes(x = Category, y = Games, fill = Category)) +
-        geom_bar(stat = "identity", width = 0.7) +
-        labs(title = "Kraken vs. Penguins", x = "Category", y = "Games") +
-        scale_fill_manual(values = c("Pitt_wins" = "#FFB81C", "Pitt_losses" = "#CC9612",
-                                     "Sea_wins" = "#75aadb", "Sea_losses" = "#4c7a9d")) + 
-        theme(axis.line.x = element_line(color = "black", size = 1, linetype = "solid"))
-      
-      barplot <- barplot + coord_cartesian(ylim = c(-50, 50)) + 
-      geom_hline(yintercept = 0, color = "black", size = 0.3)
-      plotly::ggplotly(barplot)
-    })
-  }
-=======
 trav_and_res_df <- read.csv("trav_and_res.csv")  # set working directory to source file location.
 pitt_df <- trav_and_res_df %>% filter(Sea.or.Pitt == "Pitt") %>% arrange(distance.from.arena.in.miles) %>% mutate(row = row_number())
 sea_df <- trav_and_res_df %>% filter(Sea.or.Pitt == "Sea") %>% arrange(distance.from.arena.in.miles) %>% mutate(row = row_number())
@@ -115,7 +58,7 @@ server <- function(input, output){
     
     us_states <- map_data("state")
     
-  
+    
     
     states_with_arena <- subset(us_states, region %in% c("arizona", 
                                                          "california", 
@@ -159,24 +102,24 @@ server <- function(input, output){
     is_pitt <- subset(us_states, region %in% c("pennsylvania"))
     
     far_from_pitt <- subset(us_states, region %in% c("texas", 
-                                                    "colorado", 
-                                                    "arizona"))
+                                                     "colorado", 
+                                                     "arizona"))
     
     close_to_pitt <- subset(us_states, region %in% c("new york", 
-                                                    "new jersey", 
-                                                    "district of columbia", 
-                                                    "ohio",
-                                                    "illinois",
-                                                    "missouri",
-                                                    "tennessee",
-                                                    "north carolina",
-                                                    "florida"))
+                                                     "new jersey", 
+                                                     "district of columbia", 
+                                                     "ohio",
+                                                     "illinois",
+                                                     "missouri",
+                                                     "tennessee",
+                                                     "north carolina",
+                                                     "florida"))
     
     very_far_from_pitt <- subset(us_states, region %in% c("california", 
-                                                         "nevada",
-                                                         "washington"))
+                                                          "nevada",
+                                                          "washington"))
     
-  
+    
     
     if (input$team_selector1 == "Seattle Kraken") {
       mapplot <- ggplot() +
@@ -201,31 +144,42 @@ server <- function(input, output){
         labs(title = "Neutral Plot of All States") 
     }
     
-   
+    
     return(ggplotly(mapplot))
   })
   
-  output$lineplot <- renderPlotly({
+  Sea_away_df <- sea_df %>%
+    filter(Away.Team == "Seattle Kraken") %>%
+    summarize(distance.from.arena.in.miles, W.or.L) %>% 
+    mutate(nth_away_games = row_number())
+  
+  Pitt_away_df <- pitt_df %>%
+    filter(Away.Team == "Pittsburgh Penguins") %>%
+    summarize(distance.from.arena.in.miles, W.or.L) %>% 
+    mutate(nth_away_games = row_number())
+  
+  
+  output$scatterplot <- renderPlotly({
     
     if (input$team_selector3 == "Seattle Kraken") {
-      lineplot <- ggplot() +
-        geom_line(data = sea_df, aes(x = row, y = distance.from.arena.in.miles)) + 
-        labs(title = "Sea")
+      scatterplot <- ggplot(Sea_away_df, aes(x = nth_away_games, y = distance.from.arena.in.miles, color = W.or.L, text = W.or.L)) +
+        geom_point() +
+        scale_color_manual(values = c("Sea W " = "#85c1ff", "Sea L" = "#416481")) +
+        labs(title = "Travel Distance and n-th Away Games")
     } else if (input$team_selector3 == "Pittsburgh Penguins") {
-      lineplot <- ggplot() +
-        geom_line(data = pitt_df, aes(x = row, y = distance.from.arena.in.miles)) + 
-        labs(title = "Pitt")
+      scatterplot <- ggplot(Pitt_away_df, aes(x = nth_away_games, y = distance.from.arena.in.miles, color = W.or.L, text = W.or.L)) +
+        geom_point() +
+        scale_color_manual(values = c("Pitt W" = "#FFB81C", "Pitt L" = "#CC9612")) +
+        labs(title = "Travel Distance and n-th Away Games")
     } else {
-      lineplot <- ggplot() +
-        geom_line(data = sorted_df, aes(x = row, y = distance.from.arena.in.miles)) + 
-        labs(title = "Both")
+      scatterplot <- ggplot() +
+        geom_point(data = Sea_away_df, aes(x = nth_away_games, y = distance.from.arena.in.miles, color = W.or.L, text = W.or.L)) +
+        geom_point(data = Pitt_away_df, aes(x = nth_away_games, y = distance.from.arena.in.miles, color = W.or.L, text = W.or.L)) +
+        scale_color_manual(values = c("Sea W " = "#85c1ff", "Sea L" = "#416481", "Pitt W" = "#FFB81C", "Pitt L" = "#CC9612")) +
+        labs(title = "Travel Distance and n-th Away Games")
     }
     
-    
-    return(ggplotly(lineplot))
+    return(ggplotly(scatterplot, tooltip = "text"))
     
   })
-  
-  
 }
->>>>>>> f04815ae81d3d26c8fa7b17d173543fe72c7f77c
--- ui.R
+++ ui.R
@@ -2,11 +2,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(plotly)
-<<<<<<< HEAD
-library(shiny)
-=======
 library(maps)
->>>>>>> f04815ae81d3d26c8fa7b17d173543fe72c7f77c
 
 trav_and_res_df <- read.csv("trav_and_res.csv")
 
@@ -14,8 +10,12 @@ trav_and_res_df <- read.csv("trav_and_res.csv")
 ## OVERVIEW TAB INFO
 
 overview_tab <- tabPanel("Overview Tab Title",
-                         h1("Some title"),
-                         p("some explanation")
+                         tags$img(src = "https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/penguins-seattle-kraken-1638806374.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*", height = "475px"),
+                         h1("Introduction"),
+                         p("The major question we are answering is how much more does a team from the Western Conference of the National Hockey League travel in comparison to a team from the Eastern Conference. In addition, we are hoping to see if there is a correlation between this travel and in-game success. The data came from Google Maps which showed how far each team traveled in miles from one arena to another and our Team and game results came from fixture downloads."), 
+                         p("There were no ethical limitations as the results of hockey games don’t have a bias – one team must win and the other must lose. However, something to note is we did not know the exact flight path, what hotel, and what driving routes the teams took. In addition, we didn’t know if a team would drive to a nearby arena like New York to New Jersey for example. Another example could be Pittsburgh going straight from a city like San Jose to Anaheim for back-to-back games instead of returning home in between."), 
+                         p("To combat this logistical issue we just decided to take the distance in between arenas in miles, and assumed for every away game they left directly from their home arena. We were forced to improvise due to our lack of access to the team's travel routes. So for the purpose of this assignment, and due to knowing the exact travel routes we always took distances from the home arena and calculated accordingly."), 
+                         p("Links: https://fixturedownload.com/results/nhl-2022/seattle-kraken, https://fixturedownload.com/results/nhl-2022/pittsburgh-penguins")
 )
 
 ## VIZ 1 TAB INFO
@@ -44,11 +44,7 @@ viz_1_tab <- tabPanel("Viz 1 tab title",
 
 ## VIZ 2 TAB INFO
 ## Basic bar graph of total wins for the two teams
-<<<<<<< HEAD
-## Also, we could have 4 bars, Pitt W and Pitt L and Seattle W and Seattle L. 
-=======
 ## Also, we could have 4 bars, Pitt W and Pitt L and Seattle W and Seattle L.
->>>>>>> f04815ae81d3d26c8fa7b17d173543fe72c7f77c
 
 viz_2_sidebar <- sidebarPanel(
   h2("Choose Your Team!"),
@@ -112,12 +108,42 @@ viz_3_sidebar <- sidebarPanel(
     label = "Select a Team:",
     choices = c("Pittsburgh Penguins", "Seattle Kraken", "Both"),
     selected = "Both"
+  ), 
+  conditionalPanel(
+    condition = "input.team_selector3 == 'Pittsburgh Penguins'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Pitt wins: 43%"),
+      tags$p("Pitt losses: 57%"),
+      tags$p("Out of total 41 away games")
+    )
+  ),
+  conditionalPanel(
+    condition = "input.team_selector3 == 'Seattle Kraken'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Sea wins: 61%"),
+      tags$p("Sea losses: 39%"),
+      tags$p("Out of total 41 away games")
+    )
+  ),
+  conditionalPanel(
+    condition = "input.team_selector3 == 'Both'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Sea wins: 61%"),
+      tags$p("Sea losses: 39%"),
+      tags$p("Pitt wins: 43%"),
+      tags$p("Pitt losses: 57%"),
+      tags$p("Out of total 41 away games")
+    )
   )
 )
 
+
 viz_3_main_panel <- mainPanel(
-  h2("Vizualization 3 Title"),
-  plotlyOutput(outputId = "lineplot")
+  h2("Away Games"),
+  plotlyOutput(outputId = "scatterplot")
 )
 
 viz_3_tab <- tabPanel("Viz 3 tab title",
@@ -130,9 +156,21 @@ viz_3_tab <- tabPanel("Viz 3 tab title",
 ## CONCLUSIONS TAB INFO
 
 conclusion_tab <- tabPanel("Conclusion Tab Title",
-                           h1("Some title"),
-                           p("some conclusions")
-)
+                           h1("Conclusion/Summary"),
+                           p("After carefully analyzing the data several key takeaways were noted from our project.
+First, although not all 32 teams were analyzed, our comparison of the
+Seattle Kraken and Pittsburgh Penguins makes it evident that traveling less does not always
+lead to a team winning more games. This is seen in our bar graph as the Seattle Kraken had a heavier travel schedule but won more games than the Penguins (47 wins compared to
+41)."), p("Additionally, we also noticed that there is a heavier concentration of Arenas on the
+east coast. We see that in the Northeast corner of the United States of America, it is
+significantly more dense in terms of Arenas to land, while the west coast is more
+spread out. This is a key reason as to why the travel load for a team like the Pittsburgh
+The Penguins are going to be much less than a team like the Seattle Kraken."), 
+                           p("Lastly, it is evident that 
+Pittsburgh traveled less as on the line plot of travel distance we can see that if the sum of total 
+travel in miles is taken, Pittsburgh had significantly less. However, it is important to note that 
+this information needs to be taken lightly as mentioned in the intro, since we are not sure of the exact 
+flight path, hotel accommodations, and road paths."), p("Our mileage was calculated based on the distance exactly from one arena to the other in miles rather than the exact travel plan that the team followed. Overall however, it is evident that traveling less doesn’t always mean having a more fortuitous season, that the NHL is more densely populated with arenas in the Northeast portion of the United States, and that Pittsburgh had a significantly less invasive travel schedule than the Seattle Kraken."))
 
 
 
