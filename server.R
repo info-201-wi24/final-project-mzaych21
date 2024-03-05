dff0be01b78ed3474e9dcc0472e683592a07e76e
YunsuHa <yh254@uw.edu>
2024-03-04 19:40:01 -0700

    Revised #3 graph and added intro and conclusion

--- server.R
+++ server.R
@@ -1,6 +1,56 @@
+# Run them all in the R Console.
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(shiny)
 
-server <- function(input, output){
+trav_and_res_df <- read.csv("trav_and_res.csv") # Set work
   
-  # TODO Make outputs based on the UI inputs here
-  
-}
+  server <- function(input, output){
+    
+    # TODO Make outputs based on the UI inputs here
+    output$barplot <- renderPlotly({
+      selected_team_data <- reactive({
+        if (input$team_selector == "Seattle Kraken") {
+          filtered_data <- trav_and_res_df %>%
+            summarize(
+              Sea_wins = sum(grepl("Sea W", W.or.L)),
+              Sea_losses = -sum(grepl("Sea L", W.or.L))
+            )
+        } else if (input$team_selector == "Pittsburgh Penguins") {
+          filtered_data <- trav_and_res_df %>%
+            summarize(
+              Pitt_wins = sum(grepl("Pitt W", W.or.L)),
+              Pitt_losses = -sum(grepl("Pitt L", W.or.L))
+            )
+        } else {
+          filtered_data <- trav_and_res_df %>% 
+            summarize(
+              Pitt_wins = sum(grepl("Pitt W", W.or.L)),
+              Pitt_losses = -sum(grepl("Pitt L", W.or.L)),
+              Sea_wins = sum(grepl("Sea W", W.or.L)),
+              Sea_losses = -sum(grepl("Sea L", W.or.L))
+            )
+        }
+        return(filtered_data)
+      })
+      
+      wins_losses <- reactive({
+        selected_team_data() %>%
+          pivot_longer(cols = everything(),
+                       names_to = "Category", values_to = "Games")
+      })
+      
+      barplot <- ggplot(wins_losses(), aes(x = Category, y = Games, fill = Category)) +
+        geom_bar(stat = "identity", width = 0.7) +
+        labs(title = "Kraken vs. Penguins", x = "Category", y = "Games") +
+        scale_fill_manual(values = c("Pitt_wins" = "#FFB81C", "Pitt_losses" = "#CC9612",
+                                     "Sea_wins" = "#75aadb", "Sea_losses" = "#4c7a9d")) + 
+        theme(axis.line.x = element_line(color = "black", size = 1, linetype = "solid"))
+      
+      barplot <- barplot + coord_cartesian(ylim = c(-50, 50)) + 
+      geom_hline(yintercept = 0, color = "black", size = 0.3)
+      plotly::ggplotly(barplot)
+    })
+  }
--- ui.R
+++ ui.R
@@ -1,3 +1,10 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(shiny)
+
+trav_and_res_df <- read.csv("trav_and_res.csv")
 
 
 ## OVERVIEW TAB INFO
@@ -27,15 +34,53 @@ viz_1_tab <- tabPanel("Viz 1 tab title",
 )
 
 ## VIZ 2 TAB INFO
+## Basic bar graph of total wins for the two teams
+## Also, we could have 4 bars, Pitt W and Pitt L and Seattle W and Seattle L. 
 
 viz_2_sidebar <- sidebarPanel(
-  h2("Options for graph"),
-  #TODO: Put inputs for modifying graph here
+  h2("Choose Your Team!"),
+  selectInput(
+    inputId = "team_selector",
+    label = "Select a Team:",
+    choices = c("Pittsburgh Penguins", "Seattle Kraken", "Both"),
+    selected = "Both"
+  ),
+  conditionalPanel(
+    condition = "input.team_selector == 'Pittsburgh Penguins'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Pitt wins: 50%"),
+      tags$p("Pitt losses: 50%"),
+      tags$p("Out of total 82 games")
+    )
+  ),
+  conditionalPanel(
+    condition = "input.team_selector == 'Seattle Kraken'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Sea wins: 43%"),
+      tags$p("Sea losses: 57%"),
+      tags$p("Out of total 82 games")
+    )
+  ),
+  conditionalPanel(
+    condition = "input.team_selector == 'Both'",
+    tags$div(
+      h4("Percentage:"),
+      tags$p("Sea wins: 43%"),
+      tags$p("Sea losses: 57%"),
+      tags$p("Pitt wins: 50%"),
+      tags$p("Pitt losses: 50%"),
+      tags$p("Out of total 82 games")
+    )
+  )
 )
 
+
 viz_2_main_panel <- mainPanel(
-  h2("Vizualization 2 Title"),
+  h2("Total Wins and Losses"),
   # plotlyOutput(outputId = "your_viz_1_output_id")
+  plotlyOutput(outputId = "barplot")
 )
 
 viz_2_tab <- tabPanel("Viz 2 tab title",
